ba7d5c46887ae913beb3dd1d010b6f07b22fa28b
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2014-10-16 12:22:54 -0500

    Now controlling the turtle sim through a full figure-8.  Angular velocity is unsatisfying...

--- src/turtlesim_control.py
+++ src/turtlesim_control.py
@@ -13,7 +13,6 @@ max_time_s = 10.0
 
 frame_time_s = 0.02
 
-#control_pub as rospy.Publisher
 control_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
 
 
@@ -21,10 +20,15 @@ def turtlesim_control():
 
     rospy.init_node("turtlesim_control", anonymous=True)
 
+    max_time_s = rospy.get_param("~max_time_s", 10.0)
+    print "T = " + str(max_time_s)
+
     # Wait for the turtle's teleport service to start up, then move on to orient it.
+    print "Waiting for turtle service..."
     rospy.wait_for_service("/turtle1/teleport_absolute")
     orient_turtle_service = rospy.ServiceProxy("/turtle1/teleport_absolute", TeleportAbsolute)
     orient_turtle_service(5.0, 5.0, 0.0)
+    print "Turtle position set!"
 
     rospy.Timer(rospy.Duration(frame_time_s), timer_callback)
 
@@ -32,16 +36,30 @@ def turtlesim_control():
 
 
 twist_msg = Twist()
+prev_theta_rad = 0.0
 
 def timer_callback(event):
-    global time_s, control_pub
+    global time_s, control_pub, prev_theta_rad
 
     theta_rad = (2.0 * math.pi * time_s) / max_time_s
 
-    twist_msg.linear.x = 3.0 * math.sin(2.0 * theta_rad)
-    twist_msg.linear.y = 3.0 * math.sin(theta_rad)
+    x_t = 3.0 * math.sin(2.0 * theta_rad)
+    y_t = 3.0 * math.sin(theta_rad)
 
-    twist_msg.angular.z = math.sin(0.25 * theta_rad)
+    v_x_t = ((12.0 * math.pi) / max_time_s) * math.cos(2.0 * theta_rad)
+    v_y_t = ((6.0 * math.pi) / max_time_s) * math.cos(theta_rad)
+
+    twist_msg.linear.x = math.sqrt(v_x_t**2 + v_y_t**2)
+
+    # This is completely unsatisfying, because I wanted this to be solved
+    # more elegantly, but I couldn't figure it out...
+    ang_vel_rad_per_s = 2.0 * (theta_rad - prev_theta_rad) / frame_time_s
+    if time_s > (max_time_s * 0.5):
+        ang_vel_rad_per_s = -ang_vel_rad_per_s
+
+    twist_msg.angular.z = ang_vel_rad_per_s
+
+    prev_theta_rad = theta_rad
 
     time_s += frame_time_s
     if time_s > max_time_s:
