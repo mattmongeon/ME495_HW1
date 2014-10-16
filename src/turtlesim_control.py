93972254fa46d383dd49075af4ce8e0fd9beea7c
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2014-10-15 22:26:19 -0500

    First pass at controlling turtle.  Kinematics are wrong.  They will come soon enough.

--- src/turtlesim_control.py
+++ src/turtlesim_control.py
@@ -13,7 +13,8 @@ max_time_s = 10.0
 
 frame_time_s = 0.02
 
-control_pub = None
+#control_pub as rospy.Publisher
+control_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
 
 
 def turtlesim_control():
@@ -22,28 +23,31 @@ def turtlesim_control():
 
     # Wait for the turtle's teleport service to start up, then move on to orient it.
     rospy.wait_for_service("/turtle1/teleport_absolute")
-    orient_turtle_service = rospy.ServiceProxy("/turtle1/teleport_absolute", turtlesim.srv.TeleportAbsolute)
-    orient_turtle_service(0.0, 0.0, 0.0)
+    orient_turtle_service = rospy.ServiceProxy("/turtle1/teleport_absolute", TeleportAbsolute)
+    orient_turtle_service(5.0, 5.0, 0.0)
 
-    control_pub = rospy.Publisher("/turtle1/cmd_vel", Twist)
     rospy.Timer(rospy.Duration(frame_time_s), timer_callback)
 
     rospy.spin()
 
 
-twist_msg = geometry_msgs.msg.Twist()
+twist_msg = Twist()
 
 def timer_callback(event):
-    x = 3.0 * math.sin((4.0 * math.pi * time_s) / max_time_s)
-    y = 3.0 * math.sin((2.0 * math.pi * time_s) / max_time_s)
+    global time_s, control_pub
+
+    theta_rad = (2.0 * math.pi * time_s) / max_time_s
+
+    twist_msg.linear.x = 3.0 * math.sin(2.0 * theta_rad)
+    twist_msg.linear.y = 3.0 * math.sin(theta_rad)
+
+    twist_msg.angular.z = math.sin(0.25 * theta_rad)
 
     time_s += frame_time_s
     if time_s > max_time_s:
         time_s = 0.0
 
-    twist_msg.linear.x = x
-    twist_msg.linear.y = y
-    contro_pub.publish(twist_msg)
+    control_pub.publish(twist_msg)
 
 
 def main(args):
