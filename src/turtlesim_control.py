d9eadf52bc5caae5e5ef38211d1593e7e29faa44
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2014-10-17 13:23:44 -0500

    Added comments.

--- src/turtlesim_control.py
+++ src/turtlesim_control.py
@@ -8,15 +8,22 @@ from geometry_msgs.msg import Twist
 from turtlesim.srv import TeleportAbsolute
 
 
+# Timing variables for use in the real-time loop as well as
+# for initializing our node.
 time_s = 0.0
 max_time_s = 10.0
 frame_time_s = 0.01  # Just for kicks we will run at 100 Hz.
 
+
+# Just creating a single message so we don't have to waste time
+# constructing a new message over and over again.
 twist_msg = Twist()
 
 control_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
 
 
+# Computes the x and y components of the linear velocity by taking the
+# time derivatives of xd(t) and yd(t).
 def get_linear_velocities_m_per_s(cycle_pos):
     v_x_t = ((12.0 * math.pi) / max_time_s) * math.cos(2.0 * cycle_pos)
     v_y_t = ((6.0 * math.pi) / max_time_s) * math.cos(cycle_pos)
@@ -24,6 +31,8 @@ def get_linear_velocities_m_per_s(cycle_pos):
     return v_x_t, v_y_t
 
 
+# Calculates angular velocity by calculating the first time derivative of
+# arctan(v_y_t/v_x_t).
 def get_angular_velocity_rad_per_s(cycle_pos):
     ang_vel_rad_per_s = 3.0 * math.sin(cycle_pos) + math.sin(3.0 * cycle_pos)
     ang_vel_rad_per_s *= 4.0 * math.pi
@@ -33,6 +42,8 @@ def get_angular_velocity_rad_per_s(cycle_pos):
     return ang_vel_rad_per_s
 
 
+# Our main initialization function.  Sets up the turtle's position
+# and orientation, sets the value for T, and begins the main loop.
 def turtlesim_control():
 
     global max_time_s
@@ -60,6 +71,9 @@ def turtlesim_control():
     rospy.spin()
 
 
+# Essentially our real-time loop.  Is invoked at 100 Hz, updates the
+# linear and angular velocities, and publishes them for the turtle
+# sim to execute.
 def timer_callback(event):
     global time_s, control_pub
 
