4880f544449e70b963aa3cb1c8d046b31eaa00b8
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2014-10-17 10:14:57 -0500

    Finally figured out what was wrong with some math I was doing earlier.  Figure 8 looks much better.

--- src/turtlesim_control.py
+++ src/turtlesim_control.py
@@ -10,10 +10,27 @@ from turtlesim.srv import TeleportAbsolute
 
 time_s = 0.0
 max_time_s = 10.0
+frame_time_s = 0.01  # Just for kicks we will run at 100 Hz.
 
-frame_time_s = 0.02
+twist_msg = Twist()
 
 control_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+
+
+def get_linear_velocities_m_per_s(cycle_pos):
+    v_x_t = ((12.0 * math.pi) / max_time_s) * math.cos(2.0 * cycle_pos)
+    v_y_t = ((6.0 * math.pi) / max_time_s) * math.cos(cycle_pos)
+
+    return v_x_t, v_y_t
+
+
+def get_angular_velocity_rad_per_s(cycle_pos):
+    ang_vel_rad_per_s = 3.0 * math.sin(cycle_pos) + math.sin(3.0 * cycle_pos)
+    ang_vel_rad_per_s *= 4.0 * math.pi
+
+    ang_vel_rad_per_s /= max_time_s * (math.cos(2.0 * cycle_pos) + 4.0 * math.cos(4.0 * cycle_pos) + 5.0)
+
+    return ang_vel_rad_per_s
 
 
 def turtlesim_control():
@@ -27,7 +44,13 @@ def turtlesim_control():
     print "Waiting for turtle service..."
     rospy.wait_for_service("/turtle1/teleport_absolute")
     orient_turtle_service = rospy.ServiceProxy("/turtle1/teleport_absolute", TeleportAbsolute)
-    orient_turtle_service(5.0, 5.0, 0.0)
+
+    # The initial orientation should be based on the theta determined from the
+    # initial velocities at t=0.
+    v_x_t, v_y_t = get_linear_velocities_m_per_s(0.0)
+    init_theta_rad = math.atan2(v_y_t, v_x_t)
+    orient_turtle_service(5.0, 5.0, init_theta_rad)
+
     print "Turtle position set!"
 
     rospy.Timer(rospy.Duration(frame_time_s), timer_callback)
@@ -35,29 +58,15 @@ def turtlesim_control():
     rospy.spin()
 
 
-twist_msg = Twist()
-prev_theta_rad = 0.0
-
 def timer_callback(event):
-    global time_s, control_pub, prev_theta_rad
+    global time_s, control_pub
 
-    theta_rad = (2.0 * math.pi * time_s) / max_time_s
-    delta_theta_rad = (2.0 * math.pi * frame_time_s) / max_time_s
+    cycle_pos = (2.0 * math.pi * time_s) / max_time_s
 
-    v_x_t = ((12.0 * math.pi) / max_time_s) * math.cos(2.0 * theta_rad)
-    v_y_t = ((6.0 * math.pi) / max_time_s) * math.cos(theta_rad)
-
+    v_x_t, v_y_t = get_linear_velocities_m_per_s(cycle_pos)
     twist_msg.linear.x = math.sqrt(v_x_t**2 + v_y_t**2)
 
-    # This is completely unsatisfying.  I wanted this to be solved
-    # more elegantly, but I couldn't figure it out...
-    ang_vel_rad_per_s = 2.0 * delta_theta_rad / frame_time_s
-    if time_s > (max_time_s * 0.5):
-        ang_vel_rad_per_s = -ang_vel_rad_per_s
-
-    twist_msg.angular.z = ang_vel_rad_per_s
-
-    prev_theta_rad = theta_rad
+    twist_msg.angular.z = get_angular_velocity_rad_per_s(cycle_pos)
 
     time_s += frame_time_s
     if time_s > max_time_s:
